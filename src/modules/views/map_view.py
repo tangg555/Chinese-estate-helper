57ecab8ca8f60e9cbd2caf959c3c6004d2a78eee
Chen Tang <travistang@foxmail.com>
2021-09-17 21:27:47 +0100

    更改图形界面。

--- src/modules/views/map_view.py
+++ src/modules/views/map_view.py
@@ -1,6 +1,4 @@
-from pyecharts import options as opts
-from pyecharts.charts import Map
-from pyecharts.faker import Faker
+import folium
 from src.common.constants import VIEW_DIR
 import os
 
@@ -8,13 +6,21 @@ import os
 class MapView(object):
     _class_name = "Map View"
     render_path = f'{VIEW_DIR}/render.html'
+    city_dict = ''
+
 
     @classmethod
     def city_draw(cls, city, key_vals):
+        folium.Map(location=[31.2389, 121.4992])
         dir_ = os.path.dirname(cls.render_path)
         os.makedirs(dir_, exist_ok=True)
 
-        Map().add(city, [list(one) for one in key_vals], city).set_global_opts(
-            title_opts=opts.TitleOpts(title=f"{city}市地图"), visualmap_opts=opts.VisualMapOpts()
-        ).render(path=cls.render_path)
+        vals = [one[1] for one in key_vals]
+        map = Map()
+        map.add(city, key_vals, city)
+        map.set_global_opts(
+            title_opts=opts.TitleOpts(title=f"{city}市地图"),
+            visualmap_opts=opts.VisualMapOpts()
+        )
+        map.render(path=cls.render_path)
 
--- tasks/lianjia_mysql_task.py
+++ tasks/lianjia_mysql_task.py
@@ -12,6 +12,5 @@ class LianJiaSqliteTask(object):
         downloader.insert_houses(city)
 
 
-
 if __name__ == '__main__':
     LianJiaSqliteTask.run()
